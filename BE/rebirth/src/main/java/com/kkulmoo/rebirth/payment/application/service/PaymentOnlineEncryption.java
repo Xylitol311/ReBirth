5edb32d1d30da0397eac90403ed7aa7128a01002
Park <sk05432@naver.com>
2025-03-30 19:09:26 +0900

    :sparkles: feat : 온라인 결제 로직 추가

--- BE/rebirth/src/main/java/com/kkulmoo/rebirth/payment/application/service/PaymentOnlineEncryption.java
+++ BE/rebirth/src/main/java/com/kkulmoo/rebirth/payment/application/service/PaymentOnlineEncryption.java
@@ -1,4 +1,5 @@
 package com.kkulmoo.rebirth.payment.application.service;
+
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,9 @@ import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.util.Base64;
 
-//일회용 토큰 생성 및 디코드
 @Service
-public class PaymentEncryption {
+public class PaymentOnlineEncryption {
+
 
     @Value("${token.secret.key}")
     private String secretKey; // 서명용 키 (HMAC)
@@ -23,19 +24,13 @@ public class PaymentEncryption {
 
     private static final long EXPIRATION_TIME = 5 * 60 * 1000; // 5분
 
-    @PostConstruct
-    private void validateKeys() {
-        if (aesKey.length() != 16 && aesKey.length() != 24 && aesKey.length() != 32) {
-            throw new IllegalArgumentException("AES_KEY는 16, 24, 32바이트여야 합니다.(128, 192 또는 256비트)");
-        }
-    }
 
-    public String generateOneTimeToken(String permanentToken, int userId) throws Exception {
+    // 가맹점하고 가격 정보 담아서 토큰 생성 ( AES 처리만 하기 )
+    public String generateQRToken(String merchantName, int amount) throws Exception {
         long expiration = System.currentTimeMillis() + EXPIRATION_TIME;
         String iv = generateIV();
 
-        // permanentToken과 userId를 함께 암호화
-        String encryptedData = encryptAES(permanentToken + "|" + userId, aesKey, iv);
+        String encryptedData = encryptAES(merchantName + "|" + Integer.toString(amount), aesKey, iv);
 
         String data = encryptedData + "|" + iv + "|" + expiration;
         String signature = generateHMAC(data, secretKey);
@@ -43,8 +38,9 @@ public class PaymentEncryption {
         return Base64.getUrlEncoder().withoutPadding().encodeToString((data + "|" + signature).getBytes(StandardCharsets.UTF_8));
     }
 
-    public String[] validateOneTimeToken(String token) throws Exception {
 
+    // 0 : 가맹점 정보, 1 : 가격 던지기
+    public String[] validateQRToken(String token) throws Exception {
         String decoded = new String(Base64.getUrlDecoder().decode(token), StandardCharsets.UTF_8);
         String[] parts = decoded.split("\\|");
 
@@ -55,24 +51,68 @@ public class PaymentEncryption {
         long expiration = Long.parseLong(parts[2]);
         String signature = parts[3];
 
-
+        //만료 처리로 에러 던지기
         if (System.currentTimeMillis() > expiration) return null;
 
         String data = encryptedData + "|" + iv + "|" + expiration;
         String expectedSignature = generateHMAC(data, secretKey);
 
-
         if (!expectedSignature.equals(signature)) return null;
 
-        // 복호화 후 permanentToken, userId 추출
+        // 복호화 후 가맹점 이름하고 가격 던지기
         String decryptedData = decryptAES(encryptedData, aesKey, iv);
         String[] decryptedParts = decryptedData.split("\\|");
 
         if (decryptedParts.length != 2) return null;
 
 
+        return decryptedParts;
+
+    }
+
+    //가맹점, 가격, 영구 토큰 담아서 생성해야함
+// 가맹점하고 가격 정보 담아서 토큰 생성 ( AES 처리만 하기 )
+    public String generateOnlineToken(String merchantName, int amount, String token) throws Exception {
+        long expiration = System.currentTimeMillis() + EXPIRATION_TIME;
+        String iv = generateIV();
+
+        String encryptedData = encryptAES(token + "|" + merchantName + "|" + Integer.toString(amount), aesKey, iv);
+
+        String data = encryptedData + "|" + iv + "|" + expiration;
+        String signature = generateHMAC(data, secretKey);
+
+        return Base64.getUrlEncoder().withoutPadding().encodeToString((data + "|" + signature).getBytes(StandardCharsets.UTF_8));
+    }
+
+    // 0 : 가맹점 정보, 1 : 가격 던지기
+    public String[] validateOnlineToken(String token) throws Exception {
+        String decoded = new String(Base64.getUrlDecoder().decode(token), StandardCharsets.UTF_8);
+        String[] parts = decoded.split("\\|");
+
+        if (parts.length != 4) return null;
+
+        String encryptedData = parts[0];
+        String iv = parts[1];
+        long expiration = Long.parseLong(parts[2]);
+        String signature = parts[3];
+
+        //만료 처리로 에러 던지기
+        if (System.currentTimeMillis() > expiration) return null;
+
+        String data = encryptedData + "|" + iv + "|" + expiration;
+        String expectedSignature = generateHMAC(data, secretKey);
+
+        if (!expectedSignature.equals(signature)) return null;
+
+        // 복호화 후 가맹점 이름하고 가격하고 토큰 던지기
+        String decryptedData = decryptAES(encryptedData, aesKey, iv);
+        String[] decryptedParts = decryptedData.split("\\|");
+
+        if (decryptedParts.length != 3) return null;
+
 
         return decryptedParts;
+
     }
 
     private String encryptAES(String data, String key, String iv) throws Exception {
@@ -106,4 +146,6 @@ public class PaymentEncryption {
         new SecureRandom().nextBytes(iv);
         return Base64.getEncoder().encodeToString(iv);
     }
+
+
 }
