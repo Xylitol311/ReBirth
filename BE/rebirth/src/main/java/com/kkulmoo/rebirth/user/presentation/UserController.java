09c70ac5152ccff0e69fe39b2a07a131b42ec71e
JongWon <jongwon990806@gmail.com>
2025-03-30 15:32:38 +0900

    :sparkles: Feat. User 삭제 및 myData 가져오는 Controller

--- BE/rebirth/src/main/java/com/kkulmoo/rebirth/user/presentation/UserController.java
+++ BE/rebirth/src/main/java/com/kkulmoo/rebirth/user/presentation/UserController.java
@@ -1,4 +1,34 @@
 package com.kkulmoo.rebirth.user.presentation;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kkulmoo.rebirth.common.ApiResponseDTO.ApiResponseDTO;
+import com.kkulmoo.rebirth.common.annotation.JwtUserId;
+import com.kkulmoo.rebirth.user.application.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
+
+	private final UserService userService;
+
+
+	// @PostMapping
+	// public ResponseEntity<ApiResponseDTO<Void>> setMyData(@UserId Integer userId){
+	//
+	// }
+
+	@DeleteMapping
+	public ResponseEntity<ApiResponseDTO<Void>> deleteUser(@JwtUserId Integer userId) {
+		userService.deleteUser(userId);
+		return ResponseEntity.ok(ApiResponseDTO.success("삭제에 성공하였습니다."));
+	}
+
+
 }
