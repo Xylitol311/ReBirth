6d3e97ee73fd4509d7d514d6af2eaedfa849849e
박종원 <jongwon990806@gmail.com>
2025-04-06 13:30:18 +0900

    :sparkles: Feat. SecurityConfig 설정 추가

--- BE/rebirth/src/main/java/com/kkulmoo/rebirth/auth/config/SecurityConfig.java
+++ BE/rebirth/src/main/java/com/kkulmoo/rebirth/auth/config/SecurityConfig.java
@@ -1,21 +1,15 @@
 package com.kkulmoo.rebirth.auth.config;
 
-import com.kkulmoo.rebirth.auth.handler.CustomAccessDeniedHandler;
 import com.kkulmoo.rebirth.auth.handler.CustomAuthenticationEntryPointHandler;
 import com.kkulmoo.rebirth.auth.jwt.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
@@ -23,66 +17,23 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
-
     private final CustomAuthenticationEntryPointHandler customAuthenticationEntryPointHandler;
 
-    private final CustomAccessDeniedHandler customAccessDeniedHandler;
-
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .csrf(csrf -> csrf.disable())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(customAuthenticationEntryPointHandler)
+                );
+    return http.build();
     }
-
-    @Bean
-    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
-        return new MvcRequestMatcher.Builder(introspector);
-    }
-
-    @Bean
-    public SecurityFilterChain config(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
-        MvcRequestMatcher.Builder mvc = new MvcRequestMatcher.Builder(introspector);
-
-        // white list (Spring Security 체크 제외 목록)
-        MvcRequestMatcher[] permitAllWhiteList = {
-                mvc.pattern("/login"),
-                mvc.pattern("/api"),
-                mvc.pattern("/signup"),
-                mvc.pattern("/favicon.ico"),
-                mvc.pattern("/error")
-        };
-
-        // http request 인증 설정
-        http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(permitAllWhiteList).permitAll()
-                // 그 외 요청 체크
-                .anyRequest().authenticated()
-        );
-
-        // form login disable
-        http.formLogin(AbstractHttpConfigurer::disable);
-
-        // logout disable
-        http.logout(AbstractHttpConfigurer::disable);
-
-        // csrf disable
-        http.csrf(AbstractHttpConfigurer::disable);
-
-        // session management
-        http.sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 미사용
-        );
-
-        // before filter
-        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
-        // exception handler
-        http.exceptionHandling(conf -> conf
-                .authenticationEntryPoint(customAuthenticationEntryPointHandler)
-                .accessDeniedHandler(customAccessDeniedHandler)
-        );
-
-        // build
-        return http.build();
-    }
-
 }
