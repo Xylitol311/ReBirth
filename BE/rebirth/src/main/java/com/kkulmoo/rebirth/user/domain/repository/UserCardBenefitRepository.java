799f4cac4e783831e32985bf58096de57f1a1e81
박종원 <jongwon990806@gmail.com>
2025-04-07 12:54:12 +0900

    :bulb: Update. cardDetail

--- BE/rebirth/src/main/java/com/kkulmoo/rebirth/user/domain/repository/UserCardBenefitRepository.java
+++ BE/rebirth/src/main/java/com/kkulmoo/rebirth/user/domain/repository/UserCardBenefitRepository.java
@@ -3,8 +3,10 @@ package com.kkulmoo.rebirth.user.domain.repository;
 import com.kkulmoo.rebirth.user.domain.UserCardBenefit;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserCardBenefitRepository {
-    UserCardBenefit findByUserIdAndBenefitTemplateIdAndYearAndMonth(Integer userId, Integer benefitId, int year, int month);
+    Optional<UserCardBenefit> findByUserIdAndBenefitTemplateIdAndYearAndMonth(Integer userId, Integer benefitId, int year, int month);
     UserCardBenefit save(UserCardBenefit userCardBenefit);
 }
