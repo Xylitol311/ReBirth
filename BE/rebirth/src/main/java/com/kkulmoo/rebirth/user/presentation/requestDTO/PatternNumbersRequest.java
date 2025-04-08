af65abfc264515e3ea404aaf2e7792035078a17a
박도아 <sk05432@naver.com>
2025-04-08 15:25:07 +0900

    :wrench: 패턴로그인 requestDTO 수정

---
+++ BE/rebirth/src/main/java/com/kkulmoo/rebirth/user/presentation/requestDTO/PatternNumbersRequest.java
@@ -0,0 +1,11 @@
+package com.kkulmoo.rebirth.user.presentation.requestDTO;
+
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@Getter
+public class PatternNumbersRequest {
+    private final String patternNumbers;
+
+}
